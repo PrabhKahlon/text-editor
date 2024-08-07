b700c6faa7f9ecfd3ca45b07b1b09e2d341c1204
Prabh Kahlon <prabhk@sfu.ca>
2024-08-06 17:45:04 -0700

    Setup basic window using sdl

--- main.c
+++ main.c
@@ -1,7 +1,44 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
 
-int main(int argc, char const *argv[])
+#include <SDL.h>
+
+void sdl_cc(int code)
 {
-    printf("Hello World!\n");
+    if (code < 0) {
+        fprintf(stderr, "SDL ERROR: %s\n", SDL_GetError());
+        exit(1);
+    }
+}
+
+void* sdl_cp(void* ptr) {
+    if (ptr == NULL) {
+        fprintf(stderr, "SDL ERROR: %s\n", SDL_GetError());
+        exit(1);
+    }
+    return ptr;
+}
+
+int main(void)
+{
+    sdl_cc(SDL_Init(SDL_INIT_VIDEO));
+    SDL_Window* window = sdl_cp(SDL_CreateWindow("Text", 0, 0, 800, 600, SDL_WINDOW_RESIZABLE));
+    SDL_Renderer* renderer = sdl_cp(SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED));
+
+    bool exit = false;
+
+    while (!exit) {
+        SDL_Event event = { 0 };
+        while (SDL_PollEvent(&event)) {
+            switch (event.type) {
+            case SDL_QUIT:
+                exit = true;
+                break;
+            }
+        }
+    }
+
+    SDL_Quit();
     return 0;
 }
