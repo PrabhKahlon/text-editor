9e1ac1fd0e8addc0dddd30b0cdb3408113c4f802
Prabh Kahlon <prabhk@sfu.ca>
2024-08-07 15:05:27 -0700

    Added function to render a specific character

--- main.c
+++ main.c
@@ -22,29 +22,43 @@ void* sdl_cp(void* ptr) {
     return ptr;
 }
 
-
-
-int main(void)
+//Load selected font
+void loadFont(const char* fontFile, int fontSize, TTF_Font** font_ptr)
 {
-    sdl_cc(SDL_Init(SDL_INIT_VIDEO));
-    sdl_cc(TTF_Init());
-    SDL_Window* window = sdl_cp(SDL_CreateWindow("Text", 0, 0, 800, 600, SDL_WINDOW_RESIZABLE));
-    SDL_Renderer* renderer = sdl_cp(SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED));
+    if(font_ptr != NULL)
+    {
+        TTF_CloseFont(*font_ptr);
+        *font_ptr = NULL;
+    }
+    *font_ptr = (TTF_OpenFont(fontFile, fontSize));
+    return;
+}
 
-    TTF_Font* font = sdl_cp(TTF_OpenFont("OpenSans-Regular.ttf", 12));
-    //White color in rgba
-    SDL_Color color = {255, 255, 255, 255};
-    SDL_Surface* surface = sdl_cp(TTF_RenderText_Blended(font, "Hello, World", color));
+//Renders one specific character
+void renderChar(SDL_Renderer* renderer, const char c, TTF_Font* font, SDL_Color color) 
+{
+    SDL_Surface* surface = sdl_cp(TTF_RenderGlyph_Blended(font, c, color));
     SDL_Texture* texture = sdl_cp(SDL_CreateTextureFromSurface(renderer, surface));
     SDL_FreeSurface(surface);
-    TTF_CloseFont(font);
-
     SDL_Rect fontRect = {
         .x = 0,
         .y = 0,
         .w = surface->w,
         .h = surface->h
     };
+    sdl_cc(SDL_RenderCopy(renderer, texture, &fontRect, &fontRect));
+}
+
+int main(void)
+{
+    sdl_cc(SDL_Init(SDL_INIT_VIDEO));
+    sdl_cc(TTF_Init());
+    TTF_Font* font = NULL;
+    loadFont("OpenSans-Regular.ttf", 12, &font);
+    SDL_Window* window = sdl_cp(SDL_CreateWindow("Text", 0, 0, 800, 600, SDL_WINDOW_RESIZABLE));
+    SDL_Renderer* renderer = sdl_cp(SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED));
+    //White color in rgba
+    SDL_Color color = {255, 255, 255, 255};
 
     bool exit = false;
 
@@ -61,8 +75,7 @@ int main(void)
         sdl_cc(SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0));
 
         sdl_cc(SDL_RenderClear(renderer));
-        sdl_cc(SDL_RenderCopy(renderer, texture, &fontRect, &fontRect));
-        
+        renderChar(renderer, 'A', font, color);
         SDL_RenderPresent(renderer);
     }
 
