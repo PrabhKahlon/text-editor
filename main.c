5cb62060207d1172fbb3e37328846d60f9182a82
Prabh Kahlon <prabhk@sfu.ca>
2024-08-07 23:47:06 -0700

    Added a pen to control the position of the rendered characters

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 #include <SDL.h>
 #include <SDL_ttf.h>
 
+#include "vec.h"
 
 void sdl_cc(int code)
 {
@@ -25,7 +26,7 @@ void* sdl_cp(void* ptr) {
 //Load selected font
 void loadFont(const char* fontFile, int fontSize, TTF_Font** font_ptr)
 {
-    if(font_ptr != NULL)
+    if (font_ptr != NULL)
     {
         TTF_CloseFont(*font_ptr);
         *font_ptr = NULL;
@@ -35,18 +36,37 @@ void loadFont(const char* fontFile, int fontSize, TTF_Font** font_ptr)
 }
 
 //Renders one specific character
-void renderChar(SDL_Renderer* renderer, const char c, TTF_Font* font, SDL_Color color) 
+void renderChar(SDL_Renderer* renderer, const char c, Vec2* pos, TTF_Font* font, SDL_Color color)
 {
     SDL_Surface* surface = sdl_cp(TTF_RenderGlyph_Blended(font, c, color));
     SDL_Texture* texture = sdl_cp(SDL_CreateTextureFromSurface(renderer, surface));
-    SDL_FreeSurface(surface);
     SDL_Rect fontRect = {
         .x = 0,
         .y = 0,
         .w = surface->w,
         .h = surface->h
     };
-    sdl_cc(SDL_RenderCopy(renderer, texture, &fontRect, &fontRect));
+    SDL_Rect destRect = {
+        .x = pos->x,
+        .y = pos->y,
+        .w = surface->w,
+        .h = surface->h
+    };
+    sdl_cc(SDL_RenderCopy(renderer, texture, &fontRect, &destRect));
+    pos->x += surface->w;
+    SDL_FreeSurface(surface);
+}
+
+void renderText(SDL_Renderer* renderer, const char* text, TTF_Font* font, SDL_Color color)
+{
+    Vec2 pen = {
+        .x = 0,
+        .y = 0
+    };
+
+    for (size_t i = 0; i < strlen(text); i++) {
+        renderChar(renderer, text[i], &pen, font, color);
+    }
 }
 
 int main(void)
@@ -58,7 +78,7 @@ int main(void)
     SDL_Window* window = sdl_cp(SDL_CreateWindow("Text", 0, 0, 800, 600, SDL_WINDOW_RESIZABLE));
     SDL_Renderer* renderer = sdl_cp(SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED));
     //White color in rgba
-    SDL_Color color = {255, 255, 255, 255};
+    SDL_Color color = { 255, 255, 255, 255 };
 
     bool exit = false;
 
@@ -73,9 +93,8 @@ int main(void)
         }
 
         sdl_cc(SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0));
-
         sdl_cc(SDL_RenderClear(renderer));
-        renderChar(renderer, 'A', font, color);
+        renderText(renderer, "Hello World", font, color);
         SDL_RenderPresent(renderer);
     }
 
--- makefile
+++ makefile
@@ -2,4 +2,4 @@ CFLAGS=-Wall -Wextra -std=c11 -pedantic -ggdb `pkg-config --cflags sdl2 SDL2_ttf
 LIBS=`pkg-config --libs sdl2 SDL2_ttf` -lm
 
 text: main.c
-	  $(CC) $(CFLAGS) -o text main.c $(LIBS)
+	  $(CC) $(CFLAGS) -o text main.c vec.c $(LIBS)
