e5ec9241a1ed6dc7d7c9e49c0df95c1a8e580610
Prabh Kahlon <prabhk@sfu.ca>
2024-08-09 14:26:29 -0700

    Added very basic cursor

--- main.c
+++ main.c
@@ -10,6 +10,12 @@
 
 #define MAX_BUFFER_SIZE 1024
 
+typedef struct {
+    size_t line;
+    size_t index;
+    Vec2 pos;
+} Cursor;
+
 void sdl_cc(int code)
 {
     if (code < 0) {
@@ -53,35 +59,36 @@ SDL_Texture* cacheTexture(SDL_Renderer* renderer, TTF_Font* font, Glyph_Map* gly
     int ascent = TTF_FontAscent(font);
     int descent = TTF_FontDescent(font);
 
-    if(ascent-descent != height) {
-        height = ascent-descent;
+    if (ascent - descent != height) {
+        height = ascent - descent;
     }
-    int maxWidth = height*12;
-    int maxHeight = height*12;
+    int maxWidth = height * 12;
+    int maxHeight = height * 12;
     glyphMap->glyphHeight = height;
 
+    //This only works on little endian machine
     SDL_Surface* cacheSurface = sdl_cp(SDL_CreateRGBSurface(SDL_SWSURFACE, maxWidth, maxHeight, 32, 0x000000FF, 0x0000FF00, 0x00FF0000, 0xFF000000));
 
     //Create ASCII string to generate glyphs
     char asciiString[96] = "";
     char c = 32;
-    for(int i = 0; i < 95; i++) {
+    for (int i = 0; i < 95; i++) {
         asciiString[i] = c++;
     }
 
-    SDL_Rect cacheCursor = {.x = 0, .y = 0, .w = 0, .h = height};
+    SDL_Rect cacheCursor = { .x = 0, .y = 0, .w = 0, .h = height };
     //Generate Glyphs to cache to surface
     for (size_t i = 0; i < strlen(asciiString); i++) {
         SDL_Surface* glyphSurface = sdl_cp(TTF_RenderGlyph_Blended(font, asciiString[i], color));
         sdl_cc(SDL_SetSurfaceBlendMode(glyphSurface, SDL_BLENDMODE_NONE));
-        if(cacheCursor.x + glyphSurface->w >= maxWidth) {
+        if (cacheCursor.x + glyphSurface->w >= maxWidth) {
             cacheCursor.x = 0;
             cacheCursor.y += height;
         }
         cacheCursor.w = glyphSurface->w;
         cacheCursor.h = glyphSurface->h;
-        SDL_Rect srcRect = {0, 0, glyphSurface->w, glyphSurface->h};
-        SDL_Rect dstRect = {cacheCursor.x, cacheCursor.y, cacheCursor.w, cacheCursor.h};
+        SDL_Rect srcRect = { 0, 0, glyphSurface->w, glyphSurface->h };
+        SDL_Rect dstRect = { cacheCursor.x, cacheCursor.y, cacheCursor.w, cacheCursor.h };
         sdl_cc(SDL_BlitSurface(glyphSurface, &srcRect, cacheSurface, &dstRect));
         addGlyph(glyphMap, asciiString[i], &cacheCursor);
         cacheCursor.x += glyphSurface->w;
@@ -91,21 +98,46 @@ SDL_Texture* cacheTexture(SDL_Renderer* renderer, TTF_Font* font, Glyph_Map* gly
 
 }
 
+//Very basic cursor
+void renderCursor(SDL_Renderer* renderer, const char* text, size_t textSize, Cursor* cursor, SDL_Texture* cursorTexture, Glyph_Map* glyphMap)
+{
+    SDL_Rect destRect = {
+        .x = 0,
+        .y = 0,
+        .w = 16,
+        .h = 40
+    };
+
+    for (size_t i = 0; i < cursor->index; i++) {
+        int glyph = text[i];
+        if(glyph == 10) {
+            destRect.y += glyphMap->glyphHeight;
+            destRect.x = 0;
+        }
+        if (glyph >= 32) {
+            int glyphIndex = glyph - 32;
+            destRect.x += glyphMap->glyphs[glyphIndex]->w;
+        }
+    }
+
+    sdl_cc(SDL_RenderCopy(renderer, cursorTexture, NULL, &destRect));
+}
+
 //Renders one specific character
 void renderChar(SDL_Renderer* renderer, const char c, Vec2* pos, SDL_Texture* font, SDL_Color color, Glyph_Map* glyphMap)
 {
     //temp index
     size_t index = (int)c - 32;
-    if(c < 32) {
-        if(c == 10) {
+    if (c < 32) {
+        if (c == 10) {
             pos->y += glyphMap->glyphHeight;
             pos->x = 0;
             return;
         }
         return;
     }
-    SDL_Rect fontRect = {.x = 0, .y = 0, .w = 0, .h = 0};
-    if(index > 127) {
+    SDL_Rect fontRect = { .x = 0, .y = 0, .w = 0, .h = 0 };
+    if (index > 127) {
         index = 126;
     }
     copyRect_GS(glyphMap->glyphs[index], &fontRect);
@@ -119,7 +151,7 @@ void renderChar(SDL_Renderer* renderer, const char c, Vec2* pos, SDL_Texture* fo
     pos->x += fontRect.w;
 }
 
-void renderText(SDL_Renderer* renderer, const char* text, size_t textSize, SDL_Texture* font, SDL_Color color, Glyph_Map* glyphMap)
+void renderText(SDL_Renderer* renderer, const char* text, size_t textSize, SDL_Texture* font, SDL_Texture* cursor, Cursor* curPos, SDL_Color color, Glyph_Map* glyphMap)
 {
     Vec2 pen = {
         .x = 0,
@@ -129,6 +161,8 @@ void renderText(SDL_Renderer* renderer, const char* text, size_t textSize, SDL_T
     for (size_t i = 0; i < textSize; i++) {
         renderChar(renderer, text[i], &pen, font, color, glyphMap);
     }
+
+    renderCursor(renderer, text, textSize, curPos, cursor, glyphMap);
 }
 
 int main(void)
@@ -143,10 +177,14 @@ int main(void)
     SDL_Color color = { 255, 255, 255, 255 };
     Glyph_Map* glyphMap = createGlyphMap();
     SDL_Texture* fontTexture = cacheTexture(renderer, font, glyphMap);
+    SDL_Surface* cursorSurface = sdl_cp(SDL_CreateRGBSurface(SDL_SWSURFACE, 8, 8, 8, 0x00000000, 0x00000000, 0x00000000, 0x00000000));
+    SDL_Texture* cursorTexture = sdl_cp(SDL_CreateTextureFromSurface(renderer, cursorSurface));
 
     bool exit = false;
     char buffer[MAX_BUFFER_SIZE] = "";
     size_t buffer_size = 0;
+
+    Cursor cursor = { .index = 0, .pos = {.x = 0, .y = 0} };
 
     while (!exit) {
         SDL_Event event = { 0 };
@@ -164,6 +202,7 @@ int main(void)
                 }
                 memcpy(buffer + buffer_size, event.text.text, textSize);
                 buffer_size += textSize;
+                cursor.index += textSize;
                 break;
             }
             case SDL_KEYDOWN: {
@@ -171,12 +210,14 @@ int main(void)
                 case SDLK_BACKSPACE:
                     if (buffer_size > 0) {
                         buffer_size -= 1;
+                        cursor.index -= 1;
                     }
                     break;
                 case SDLK_RETURN: {
                     char* newLine = "\n";
                     memcpy(buffer + buffer_size, newLine, strlen(newLine));
                     buffer_size += strlen(newLine);
+                    cursor.index += strlen(newLine);
                 }
                 }
                 break;
@@ -186,7 +227,7 @@ int main(void)
 
         sdl_cc(SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0));
         sdl_cc(SDL_RenderClear(renderer));
-        renderText(renderer, buffer, buffer_size, fontTexture, color, glyphMap);
+        renderText(renderer, buffer, buffer_size, fontTexture, cursorTexture, &cursor, color, glyphMap);
         SDL_RenderPresent(renderer);
     }
     freeGlyphMap(glyphMap);
